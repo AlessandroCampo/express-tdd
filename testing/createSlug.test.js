02a5abb6fd9d7377adf07d5386b8e71473fa99c4
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-30 14:44:00 +0200

    feat: unique title with counter

--- testing/createSlug.test.js
+++ testing/createSlug.test.js
@@ -1,8 +1,15 @@
 const { test, expect } = require('@jest/globals');
 
 
-const createSlug = (string) => {
-    return string.toLowerCase().replaceAll(' ', '-')
+const createSlug = (string, array = []) => {
+    let baseSlug = string.toLowerCase().replaceAll(' ', '-');
+    let generatedSlug = '';
+    let counter = 1;
+    while (array.includes(generatedSlug)) {
+        generatedSlug = `${baseSlug}-${counter}`
+        counter++
+    }
+    return generatedSlug
 }
 
 test('the createSlug function should return a string', () => {
@@ -21,4 +28,14 @@ test('createSlug should return a string in lower case', () => {
 test('createSlug should return a string where all blank spaces are replaced by -', () => {
     const slug = createSlug('titolo di Esempio');
     expect(slug).not.toContain(" ");
+});
+
+test('createSlug should include a counter at the end, which is incremented by one whenever the slug already exists. Therefore the function should return a slug that is not contained in the array passed as seocnd parameter', () => {
+
+    const exampleTitles = ['titolo di prova', 'titolo di prova 2', 'titolo di prova', 'titolo di esempio', 'titolo di prova 2'];
+    const hopefullyUniqueTitles = exampleTitles.reduce((acc, title) => {
+        return [...acc, createSlug(title)]
+    }, [])
+    const rI = Math.floor(Math.random() * exampleTitles.length)
+    expect(hopefullyUniqueTitles).not.toContain(createSlug(exampleTitles[rI], hopefullyUniqueTitles));
 });
