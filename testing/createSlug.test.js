961fd2e20cbecc57ea1ae1783f17887e87a3e472
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-30 14:30:19 +0200

    third test

--- testing/createSlug.test.js
+++ testing/createSlug.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@jest/globals');
 
 
 const createSlug = (string) => {
-    return string.toLowerCase()
+    return string.toLowerCase().replaceAll(' ', '-')
 }
 
 test('the createSlug function should return a string', () => {
@@ -12,7 +12,13 @@ test('the createSlug function should return a string', () => {
 
 
 
-test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+test('createSlug should return a string in lower case', () => {
     const slug = createSlug('titolo di Esempio');
     expect(slug.toLowerCase()).toBe(slug);
+});
+
+
+test('createSlug should return a string where all blank spaces are replaced by -', () => {
+    const slug = createSlug('titolo di Esempio');
+    expect(slug).not.toContain(" ");
 });
