d2fc0d112a1e5f812162bb933d5f8344b28bbc8a
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-30 14:27:47 +0200

    second test

--- testing/createSlug.test.js
+++ testing/createSlug.test.js
@@ -2,10 +2,17 @@ const { test, expect } = require('@jest/globals');
 
 
 const createSlug = (string) => {
-    return 'Hello World';
+    return string.toLowerCase()
 }
 
 test('the createSlug function should return a string', () => {
     const slug = createSlug('titolo di esempio');
     expect(typeof slug).toBe('string');
+});
+
+
+
+test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+    const slug = createSlug('titolo di Esempio');
+    expect(slug.toLowerCase()).toBe(slug);
 });
